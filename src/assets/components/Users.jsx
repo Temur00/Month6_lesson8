c315d715d742941abe9df096ab7a9416d1f313bb
Temur <shernazarovtemurcom001@gmail.com>
2024-01-13 18:10:51 +0500

    has been corrected

--- src/assets/components/Users.jsx
+++ src/assets/components/Users.jsx
@@ -34,37 +34,39 @@ const Users = () => {
   }, []);
 
   return (
-    <div className="location">
-      <div className="users">
-        {users.map((user) => (
-          <div key={user.id} className="user">
-            <p>{user.id}</p>
-            <p>{user.name}</p>
-            <p>{user.username}</p>
-            <p>Email:{user.email}</p>
-            <p>Website:{user.website}</p>
-            <p>
-              Address:
-              {user.address.city},{user.address.street}
-            </p>
-            <p>Phone:{user.phone}</p>
-            <div className="buttons">
-              <button onClick={() => getUserPosts(user.id)}>Posts</button>
-              <button onClick={() => getUserTodos(user.id)}>Todos</button>
-              <button onClick={() => getUserAlbum(user.id)}>Album</button>
+    <div className="all">
+      <div className="container location">
+        <div className="users">
+          {users.map((user) => (
+            <div key={user.id} className="user">
+              <p>{user.id}</p>
+              <p>{user.name}</p>
+              <p>{user.username}</p>
+              <p>Email:{user.email}</p>
+              <p>Website:{user.website}</p>
+              <p>
+                Address:
+                {user.address.city},{user.address.street}
+              </p>
+              <p>Phone:{user.phone}</p>
+              <div className="buttons">
+                <button onClick={() => getUserPosts(user.id)}>Posts</button>
+                <button onClick={() => getUserTodos(user.id)}>Todos</button>
+                <button onClick={() => getUserAlbum(user.id)}>Album</button>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
 
-      <div className="posts">
-        {posts.map((post) => (
-          <div key={post.id} className="post">
-            <p>{post.userId}</p>
-            <p>{post.title}</p>
-            <p>{post.body}</p>
-          </div>
-        ))}
+        <div className="posts">
+          {posts.map((post) => (
+            <div key={post.id} className="post">
+              <p>{post.userId}</p>
+              <p>{post.title}</p>
+              <p>{post.body}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
